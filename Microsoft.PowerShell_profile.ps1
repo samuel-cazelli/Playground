d8f53aff96633ff8078d03ec150dbb17902ca908
Samuel Cazelli <samuelcazelli@gmail.com>
2023-08-09 08:11:35 -0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,7 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\meu.omp.json" | Invoke-Expression
 
 
-function Write-ColorOutput($ForegroundColor)
-{
+function Write-ColorOutput($ForegroundColor) {
     # save the current color
     $fc = $host.UI.RawUI.ForegroundColor
 
@@ -48,7 +47,7 @@ function Get-GitSendCurrentBranchTo {
     
     if ( $shouldSend -eq '0') {
         Write-ColorOutput green ">>>git push to $toBranch<<<"
-        #git push
+        git push
         Write-ColorOutput green ">>>git checkout $currentBranch<<<"
         git checkout $currentBranch
     } 
@@ -61,3 +60,89 @@ function Get-GitSendCurrentBranchTo {
 }
 New-Alias -Name sendTo -Value Get-GitSendCurrentBranchTo
 
+
+
+function Get-GitParentBranch {
+
+    param
+    ($currentBranch = '')
+
+    # Get the current branch name
+    if ($currentBranch -eq ''){
+        $currentBranch = git rev-parse --abbrev-ref HEAD
+    }
+
+    # Get the output of git show-branch
+    $gitShowBranch = git show-branch $currentBranch
+
+    # Filter lines containing '*'
+    $asteriskLines = $gitShowBranch | Select-String '\*'
+
+
+
+    # Filter out lines with the current branch name
+    $filteredLines = $asteriskLines | Where-Object { $_ -notmatch [regex]::Escape($currentBranch) }
+
+    # Get the first line after filtering
+    $firstLine = $filteredLines | Select-Object -First 1
+
+    # Extract the text within square brackets
+    $result = [regex]::Match($firstLine, '\[(.*?)\]').Groups[1].Value
+
+    # Remove the caret (^) and tilde (~) characters and everything after them
+    $result = $result -replace '\^.*', '' -replace '~.*', ''
+
+    $result # Output the final result
+
+}
+New-Alias -Name parentBranch -Value Get-GitParentBranch
+
+
+
+
+function Get-GitRenameMergedBranches {
+
+    param
+    ($currentBranch = '')
+
+    if ($currentBranch -eq ''){
+        $currentBranch = git rev-parse --abbrev-ref HEAD
+    }
+
+    $mergedBranches = git branch --merged $currentBranch
+
+    $mergedBranches = $mergedBranches | Select-String  '(fix*)', '(feature*)'
+
+    echo $mergedBranches
+
+    echo '----------------------------'
+
+    $mergedBranches | ForEach-Object { 
+        $choices = @(
+            [System.Management.Automation.Host.ChoiceDescription]::new("&rename")
+            [System.Management.Automation.Host.ChoiceDescription]::new("&next")
+        )
+
+        Write-Host "$currentBranch -> $_" 
+        
+        $shouldSend = $Host.UI.PromptForChoice($title, '', $choices, 1)
+        
+        if ( $shouldSend -eq '0')
+        {
+                $newName = 'merged/' + $currentBranch + '/' +  $_.ToString().Trim();
+                git branch -m $_.ToString().Trim() $newName
+        }
+    }
+}
+New-Alias -Name clearBranches -Value Get-GitRenameMergedBranches
+
+
+function Get-GitCheckoutBranch {
+    git checkout $(git branch | ForEach-Object { if ($_ -notmatch  '^\s*merge') { $_ } }  | fzf).ToString().Trim()
+}
+New-Alias -Name c -Value Get-GitCheckoutBranch
+
+function Get-GitCheckoutBranch {
+    git merge $(git branch | ForEach-Object { if ($_ -notmatch  '^\s*merge') { $_ } }  | fzf).ToString().Trim()
+}
+New-Alias -Name m -Value Get-GitCheckoutBranch
